2a6a17db703dbf3aeace5277e3777b074d622c5c
davidrast3 <david.rast@ualberta.ca>
2017-11-28 07:51:57 -0700

    added CFA script

---
+++ 35_cfa_lavaan.R
@@ -0,0 +1,107 @@
+######################################################################
+#################### Confirmatory Factor Analysis ####################
+######################################################################
+
+# Date: TODAY'S DATE
+# By: *INSERT NAME HERE*
+# Description: PROJECT DISCRIPTION
+# Version of R used: CURRENT VERSION OF R
+
+#####################################
+### Import data & load libraries ####
+#####################################
+
+## update packages then install these packages if not yet installed
+# rm(list = ls())
+update.packages(ask = FALSE, checkBuilt = TRUE)
+if(!require(pacman)){install.packages("pacman")}
+pacman::p_load(parallel, rio, tidyverse, lavaan)
+
+## load data
+
+dat <- import("https://www.dropbox.com/s/cnumemoi62ef2hz/UMKC%20IRI%20Validation%20-%202nd%20data%20collection.sav?dl=1")
+
+# check to see that you loaded the correct dataset
+View(dat)
+
+# list variables in dataset
+glimpse(dat)
+
+# NB: Alt + Shift + K --> will bring up keyboard shortcuts
+
+###############################
+########## Data Prep ##########
+###############################
+
+## CFA instructions for lavaan can be found: http://lavaan.ugent.be/tutorial/cfa.html
+## cheatsheet here too: https://github.com/jeromyanglim/lavaan-examples/blob/master/cheat-sheet-lavaan/cheat-sheet-lavaan.md
+
+
+# create new dataframes for just the focal CFA
+dat1 = dat %>% 
+              select(item1, item2, item3, item4, item5)
+
+# create more dataframes if multiple CFAs
+
+dat2 = dat %>% 
+              select(item1, item2, item3, item4, item5, item6, item7, item8, item9, item10)
+
+
+# check dataframes 
+glimpse(dat1)
+glimpse(dat2)
+
+
+### lavaan ----
+
+## cereate models
+
+# specify model
+five_item_model <-  ' factor1  =~ item1 + item2 + item3 + item4 + item5 '
+
+two_factor_model <- ' factor1  =~ item1 + item2 + item3 + item4 + item5  
+                      factor2  =~ item6 + item7 + item8 + item9 + item10 '
+
+
+saturated_model <- ' factor1  =~ item1 + item2 + item3 + item4 + item5 +
+                                 item6 + item7 + item8 + item9 + item10 '
+
+
+
+## fit model
+
+# might take a minute or two to run depending on amount of resamples
+
+fit_five_item        <- cfa(five_item_model,  data = dat1, mimic = 'Mplus')
+fit_two_factor_model <- cfa(two_factor_model, data = dat2, mimic = 'Mplus')
+fit_saturated_model  <- cfa(saturated_model,  data = dat2, mimic = 'Mplus')
+
+
+
+## fit measures ----
+
+# fit summary for 1 factor, 5 item model
+summary(fit_five_item, fit.measures=TRUE)
+round(fitMeasures(fit_five_item)[c('chisq', 'df', 'pvalue', 'cfi', 'ifi', 'tli', 'rmsea', 'srmr')], 3)
+# fitMeasures(fit_five_item) # could also run this
+
+# fit summary for 2 factor model
+summary(fit_two_factor_model, fit.measures=TRUE)
+round(fitMeasures(fit_two_factor_model)[c('chisq', 'df', 'pvalue', 'cfi', 'ifi', 'tli', 'rmsea', 'srmr')], 3)
+
+# fit summary for fully saturated, 1 factor model
+summary(fit_saturated_model, fit.measures=TRUE)
+round(fitMeasures(fit_saturated_model)[c('chisq', 'df', 'pvalue', 'cfi', 'ifi', 'tli', 'rmsea', 'srmr')], 3)
+
+
+## compare models, if needed
+# produces Chi-square difference test
+
+# compare 1 factor (5 item) model vs 2 factor (10 item) model
+anova(fit_five_item, fit_two_factor_model)
+
+# compare 1 factor (5 item) model vs fully saturated model
+anova(fit_five_item, fit_saturated_model)
+
+# compare 2 factor (10 item) model vs fully saturated model
+anova(fit_two_factor_model, fit_saturated_model)
