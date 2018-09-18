a8a6023a02ef56811c02050f08a61acfa27dab70
David <david.rast@ualberta.ca>
2018-09-18 12:06:05 -0600

    updated script

--- 01_reliability_and_efa.R
+++ 01_reliability_and_efa.R
@@ -16,7 +16,7 @@
 # rm(list = ls())
 update.packages(ask = FALSE, checkBuilt = TRUE)
 if(!require(pacman)){install.packages("pacman")}
-install_github("Displayr/flipDimensionReduction")
+pacman::p_install_gh("Displayr/flipDimensionReduction")
 pacman::p_load(parallel, rio, tidyverse, car, psych, flipDimensionReduction, corrr)
 
 # if you want reproducible analysis, use the checkpoint() command using YYYY-MM-DD format
