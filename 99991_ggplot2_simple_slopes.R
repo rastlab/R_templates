134b30e1a5a7c4d26c1e6b1e33be492adbbe46ba
David Rast <david.rast@ualberta.ca>
2019-12-09 18:07:02 -0700

    testing SS ploting from github source()

--- 99991_ggplot2_simple_slopes.R
+++ 99991_ggplot2_simple_slopes.R
@@ -3,21 +3,51 @@
 #####################################
 
 # load packages
-pacman::p_load(rio, pequod, tidyverse, janitor, QuantPsyc, lmSupport, jtools, interactions, ggstance, patchwork)
+# pacman::p_load(rio, pequod, tidyverse, janitor, QuantPsyc, lmSupport, jtools, interactions, ggstance, patchwork)
 
 # import data
-dat <- import(file.choose())
+# dat <- import(file.choose())
 
-#####################################################################################
-######################## Simple Slope Testing Automatically #########################
-#####################################################################################
+# #####################################################################################
+# ######################## Simple Slope Testing Automatically #########################
+# #####################################################################################
+# 
+# ### create x, z, w, and y columns by renaming IVs
+# dat$z <- NON-MODERATOR_HERE # x-axis variable here
+# dat$x <- MODERATOR_HERE     # moderator_1 variable here
+# dat$w <- PANEL_HERE         # moderator_2 variable here
+# dat$y <- DV_HERE            # outcome variable here
+# 
+# 
+# ## create labels for figure
+# # must use quotes for labels
+# # change labels in quotes to be what you want them to be
+# 
+# panel_a_label  <- "Panel A: YYY"         # panel A = low moderator
+# panel_b_label  <- "Panel B: ZZZ"         # panel B = high moderator
+# y_label        <- "dv_name"
+# y_range        <- c(-1.0, 1.0)           # desired numeric range of y-axis
+# y_axis_high    <- 1.0                    # high descrete numeric value displayed on y-axis
+# y_axis_low     <- -1.0                    # low descrete numeric value displayed on y-axis
+# z_label        <- "z_non-moderator_name" # X-axis variable (non-moderator)
+# z_values       <- c("low", "high")       # non-moderator values
+# z_range        <- c(-1.0, 1.0)           # non-moderator numerical values
+# modx_label     <- "x_moderator_name"     # figure legend (moderator)
+# modx_values    <- c("low", "high")       # moderator values
+# 
+# # run but only modify these if needed
+# # line type and color are set for APA 7ed
+# line_types    <- c("longdash", "solid")   # can be “solid”, “dashed”, “dotted”, “dotdash”, “longdash”, “twodash”
+# line_colors   <- c("#9E9E9E", "#000000")  # these are colorblind friendly color schemes, don't change unless needed
+# 
+# # change legend location if needed
+# # in the format of (x,y), can be any number between 0 and 1
+# # (0.8, 0.8) is upper-right corner, (0.2, 0.8) is the top left of the figure
+# legend_loc    <- c(0.2, 0.8)
 
-### create x, z, w, and y columns by renaming IVs
-dat$z <- NON-MODERATOR_HERE # x-axis variable here
-dat$x <- MODERATOR_HERE     # moderator_1 variable here
-dat$w <- PANEL_HERE         # moderator_2 variable here
-dat$y <- DV_HERE            # outcome variable here
-
+###########################################
+####### Run code but do not modify ########
+###########################################
 
 # regression
 step3 <- lm(y ~ z * x * w, data=dat)
@@ -30,36 +60,6 @@ data_plot <- probe_interaction(step3,
                                mod2 = w,
                                modx.values = "plus-minus",
                                mod2.values = "plus-minus")
-
-## create labels for figure
-# must use quotes for labels
-# change labels in quotes to be what you want them to be
-
-panel_a_label  <- "Panel A: YYY"         # panel A = low moderator
-panel_b_label  <- "Panel B: ZZZ"         # panel B = high moderator
-y_label        <- "dv_name"
-y_range        <- c(-1.0, 1.0)           # desired numeric range of y-axis
-y_axis_high    <- 1.0                    # high descrete numeric value displayed on y-axis
-y_axis_low     <- -1.0                    # low descrete numeric value displayed on y-axis
-z_label        <- "z_non-moderator_name" # X-axis variable (non-moderator)
-z_values       <- c("low", "high")       # non-moderator values
-z_range        <- c(-1.0, 1.0)           # non-moderator numerical values
-modx_label     <- "x_moderator_name"     # figure legend (moderator)
-modx_values    <- c("low", "high")       # moderator values
-
-# run but only modify these if needed
-# line type and color are set for APA 7ed
-line_types    <- c("longdash", "solid")   # can be “solid”, “dashed”, “dotted”, “dotdash”, “longdash”, “twodash”
-line_colors   <- c("#9E9E9E", "#000000")  # these are colorblind friendly color schemes, don't change unless needed
-
-# change legend location if needed
-# in the format of (x,y), can be any number between 0 and 1
-# (0.8, 0.8) is upper-right corner, (0.2, 0.8) is the top left of the figure
-legend_loc    <- c(0.2, 0.8)
-
-###########################################
-####### Run code but do not modify ########
-###########################################
 
 ### tells R to provide a plot in APA style, figure is MS ready
 apatheme = theme_bw() +
@@ -125,7 +125,7 @@ panel_b <- data_plot$interactplot$data %>%
 ####### Here are your figures ########
 ######################################
 
-panel_a
-panel_b 
+# panel_a
+# panel_b 
 
-(figure_1 <- panel_a / panel_b)
+# (figure_1 <- panel_a / panel_b)
