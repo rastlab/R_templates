3ae7a3500d72d77ad6e148763552cd060c77e80d
David <david.rast@ualberta.ca>
2021-02-08 09:40:32 -0700

    changed 'osfr' install to CRAN from github.

--- 00-osf_project.R
+++ 00-osf_project.R
@@ -11,9 +11,7 @@
 
 # Install pacman & devtools packages if necessary
 if(!"pacman" %in% rownames(installed.packages())) install.packages("pacman")
-if(!"devtools" %in% rownames(installed.packages())) install.packages("devtools")
 
-remotes::install_github("centerforopenscience/osfr")
 pacman::p_load(osfr)
 
 ###############################
